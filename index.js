cf20a9ff8da386a772a767e67352070229aafdce
hugocostav <hugocostam100@outlook.com.br>
2021-08-22 23:49:09 -0300

    displayPhoto function

--- index.js
+++ index.js
@@ -26,3 +26,30 @@ function setAttributes(element, attributes) {
     element.setAttribute(key, attributes[key]);
   }
 }
+
+/* Create elements for links and photos, add to DOM */
+function displayPhotos() {
+  imageLoaded = 0;
+  totalImages = photosArray.length;
+  // Run function for each object in photosArray
+  photosArray.forEach((photo) => {
+    // Create <a> to link to full photo
+    const item = document.createElement("a");
+    setAttributes(item, {
+      href: photo.links.html,
+      target: "_blank",
+    });
+    // Create <img> for photo
+    const img = document.createElement("img");
+    setAttributes(img, {
+      src: photo.urls.regular,
+      alt: photo.alt_description,
+      title: photo.alt_description,
+    });
+    // Event listener, check when each is finished loading
+    img.addEventListener("load", imageLoaded);
+    // Put <img> iside <a>, then put both inside imageContainer element
+    item.appendChild(img);
+    imageContainer.appendChild(item);
+  });
+}
